39127e6d6f4d7fa3caaf563cb32de01306cee583
kalpateli <tkalp@trellissoft.ai>
2023-11-30 16:51:08 +0530

    added filter functionality

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -1,67 +1,140 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { Employee } from '../Employee';
 import { EmployeesService } from '../services/employees.service';
-import { Router } from '@angular/router';
+import { filter , map} from 'rxjs';
+
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
   styleUrls: ['./employees.component.scss']
 })
 
-export class EmployeesComponent implements OnInit{
+export class EmployeesComponent implements OnInit {
 
-
-  employees : Employee[]=[];
-  employee :Employee = {
-    "id":0,
-    "firstName":"",
-    "lastName":"",
-    "email":"",
-    "mobile":"",
-    "employee_no":0,
-    "dob":"",
+  nameLength: number;
+  limit: number;
+  findData: string;
+  employees: Employee[] = [];
+  employee: Employee = {
+    "id": 0,
+    "firstName": "",
+    "lastName": "",
+    "email": "",
+    "mobile": "",
+    "employee_no": 0,
+    "dob": "",
     "address":
     {
-      "city":"",
-      "state":"",
-      "country":""
+      "city": "",
+      "state": "",
+      "country": ""
     },
-    "password":""
+    "password": ""
 
   }
 
 
-constructor(
-  private _employee : EmployeesService,
-    private router : Router
-)
-{}
+  constructor(
+    private _employee: EmployeesService
+  ) { }
 
-ngOnInit(){
-  this.getEmployeesDetails()
+  ngOnInit() {
+    this.getEmployeesDetails()
+  }
+
+  getEmployeesDetails() {
+    this._employee.getEmployeeDetails()
+      .subscribe((res) => {
+        console.log(res);
+        this.employees = res;
+      })
+  }
+
+  onCreateEmp(){
+    this._employee.postEmployeeDetails(this.employee);
+  }
+
+  onEdit(emp: Employee) {
+    this.employee = emp;
+  }
+
+  onUpdateEmp() {
+    console.log(this.employee);
+    this._employee.updateEmployeeDetails(this.employee);
+  }
+
+  onDelete(emp: Employee) {
+    if(confirm("Do you want to delete details of "+emp.firstName+" "+emp.lastName+" ?"))
+    this._employee.deleteEmployeeDetails(emp);
+  }
+
+  filterByLength(){
+    // console.log("nameLength"+this.nameLength);
+    this._employee.getEmployeeDetails()  
+    .pipe(
+      map(employees => employees
+        .filter(emp => emp.firstName && emp.firstName.length <= this.nameLength)),
+        )
+      .subscribe((res) => {
+        console.log(res);
+        this.employees = res;
+      })
+  }
+
+  filterByCity(citySlc : string){
+    if(citySlc === "all")
+    {
+      this.getEmployeesDetails();
+      
+    }
+    else
+    {
+
+      this._employee.getEmployeeDetails()  
+      .pipe(
+        map(employees => employees
+        .filter(emp => emp.address.city && emp.address.city === citySlc)),
+        )
+        .subscribe((res) => {
+          console.log(res);
+          this.employees = res;
+        })
+      }
+  }
+
+  filterByLimit(){
+
+    this._employee.getEmployeeDetails()  
+    .pipe(
+      map(employees => employees
+        .filter(emp => emp.id <= this.limit)),
+        )
+      .subscribe((res) => {
+        console.log(res);
+        this.employees = res;
+      })
+  }
+
+  searchData(){
+    if(this.findData === "")
+    {
+      this.getEmployeesDetails();
+      
+    }
+    else
+    {
+    this._employee.getEmployeeDetails()  
+    .pipe(
+      map(employees => employees
+        .filter(emp => 
+          emp.firstName == this.findData || emp.lastName == this.findData)),
+        )
+      .subscribe((res) => {
+        console.log(res);
+        this.employees = res;
+      })
+  }
+
+
 }
-
-getEmployeesDetails(){
-  this._employee.getEmployeeDetails()
-    .subscribe((res)=>{
-      console.log(res);
-      this.employees=res;
-    })
-}
-
-onEdit(emp:Employee){
-  this.employee=emp;
- }
-
-onUpdateEmp()
-{
-
-console.log(this.employee);
-  this._employee.updateEmployeeDetails(this.employee);
-}
-
-onDelete(emp : Employee){
-  this._employee.deleteEmployeeDetails(emp);
-}
-
 }
--- src/app/home/section1/section1.component.ts
+++ src/app/home/section1/section1.component.ts
@@ -1,12 +1,16 @@
-import { Component, Input , OnInit , OnChanges, ViewChild ,ElementRef, AfterViewInit} from '@angular/core';
-import { 
-  Subscription, 
-  fromEvent, 
-  interval, 
-  of, 
-  toArray, 
+import { Component, Input, OnInit, OnChanges, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
+import {
+  Observable,
+  Subscription,
+  fromEvent,
+  interval,
+  of,
+  toArray,
   take,
-  from } from 'rxjs';
+  timer,
+  from,
+  map
+} from 'rxjs';
 import { RxjsPracService } from 'src/app/services/rxjs-prac.service';
 
 @Component({
@@ -14,79 +18,252 @@ import { RxjsPracService } from 'src/app/services/rxjs-prac.service';
   templateUrl: './section1.component.html',
   styleUrls: ['./section1.component.scss']
 })
-export class Section1Component implements OnInit, AfterViewInit{
 
-  @Input() message : string ; 
+
+
+export class Section1Component implements OnInit, AfterViewInit, OnDestroy {
+
+  @Input() message: string;
+  obsMsg: any;
+  subs2 : Subscription;
+  subs3 : Subscription;
+  subs4 : Subscription;
+  members: any;
+
+  sourceSub: Subscription;
+  names : string;
+  users: any[] = [
+    { name: "abc", skill: 'Angular' , job :{ title : "Frontend"}},
+    { name: "def", skill: 'Angular' , job :{ title : "Frontend"}},
+    { name: "ghi", skill: 'Angular' , job :{ title : "Frontend"}}
+
+  ]
+  userData : any;
+  techStatus: string;
+
 
   //form Event
   @ViewChild('addBtn') addBtn: ElementRef;
 
 
-  constructor(private _rxjsPrac : RxjsPracService){
+  constructor(private _rxjsPrac: RxjsPracService) {
 
   }
 
 
   // fruits : string[] =["Apple","banana","Kiwi"]
-  
-  sourceSub : Subscription;
-  users:any[]=[
-    {name: "abc" , skill:'Angular'},
-    {name: "abc" , skill:'Angular'},
-    {name: "abc" , skill:'Angular'}
 
-  ]
-  
-  ngOnChanges(){
+
+  ngOnChanges() {
     console.log("section 1 : changed")
   }
-  
-  ngOnInit() : void {
+
+  ngOnInit(): void {
+
+    //Pluck Operator
+ 
+    
+    
+    
+    //Map Operator
+    // example:1 
+    from(this.users).pipe(
+      
+      // map(x => x?.foo?.bar)
+      map(data=>{return data.job.title}),
+      toArray()
+      )
+    .subscribe((res)=>{
+      console.log(res);
+      this.userData=res;
+    })
+
+    // example:2
+
+    // const membersDetails = interval(1000);
+    const membersDetails = from([
+      {id:1 , name:'abc'},
+      {id:2 , name:'def'},
+      {id:3 , name:'ghi'},
+      {id:4 , name:'jkl'},
+    ])
+
+    
+    this.subs4 = membersDetails.pipe(
+      map(data => {
+        // console.log('member : ',data);
+        return data.name;
+        // let modData = 'Video : '+ data;
+        // return modData;
+    })).subscribe(res=>{
+        
+        this.members = res;
+        console.log(res);
+        this._rxjsPrac.printEl(res, 'mapContainer')
+
+      }
+    )
+
+      setTimeout(()=>{
+        this.subs4.unsubscribe();
+      },10000)
+
+    //custom Observable (manual example)
+    const cusObs1 = Observable.create((observer) => {
+
+      setTimeout(() => {
+        observer.next('JavaScript') // to emit we use next
+      }, 1000)
+
+      setTimeout(() => {
+        observer.next('React')
+        // observer.error(new Error('Limit Exceed'))// to manually create error
+      }, 2000)
+
+      setTimeout(() => {
+        observer.next('CSS')
+        observer.complete()   //this completes the process and doesnt goes to the next data 
+      }, 3000)
+    })
+
+    cusObs1.subscribe((res) => {
+      // console.log(res);
+      this._rxjsPrac.printEl(res, 'elContainer')
+    },
+      (err) => {
+        console.log(err);
+        this.techStatus = 'error'
+
+      },
+      () => {
+        this.techStatus = 'completed'
+
+      })
+
+    //custom Observable (custom Interval example)
+    
+    const arr2 = ['Angular','Typescript','HTML'];
+    const cusObs2 = Observable.create((observer) => {
+      let i=0;
+      setInterval(() => {
+        // observer.next('data emit ' + i)
+        observer.next(arr2[i])
+
+        
+        // if( i >= 1)
+        // {
+        //   observer.error((err)=>{
+        //     console.log(err);
+        //   })
+        // }
+        if( i >= 2)
+        {
+          observer.complete()
+        }
+        i++;
+      }, 1000)
+    })
+
+    this.subs2 = cusObs2.subscribe((res) => {
+      // console.log(res);
+      this._rxjsPrac.printEl(res, 'intCustom')
+
+    })
+
+
+    //custom Observable (Random Names)
+
+    const arr3 = ['Angular','Typescript','HTML'];
+    const cusObs3 = Observable.create((observer) => {
+      let i=0;
+      setInterval(() => {
+        // observer.next('data emit ' + i)
+        observer.next(arr3[i])
+
+        
+        // if( i >= 1)
+        // {
+        //   observer.error((err)=>{
+        //     console.log(err);
+        //   })
+        // }
+        if( i >= 2)
+        {
+          observer.complete()
+        }
+        i++;
+      }, 1000)
+    })
+    this.subs3 = cusObs3.subscribe((res) => {
+      console.log(res);
+      // this._rxjsPrac.printEl(res, 'ranNames');
+      this.names=res
+    })
+
     //toArray (Example 01)
-    const source =interval(1000);
+    const source = interval(1000);
     this.sourceSub = source.pipe(
       take(5),
       toArray())
-    .subscribe((res)=>{
-      console.log(res);
+      .subscribe((res) => {
+        // console.log(res);
 
-      // if(res >= 8)
-      // {
-      //   this.sourceSub.unsubscribe();
-      // }
-    })
+        // if(res >= 8)
+        // {
+        //   this.sourceSub.unsubscribe();
+        // }
+      })
 
     //toArray (Example 02)
     const source2 = from(this.users);
-    source2.pipe(toArray()).subscribe((res)=>{
-      console.log(res);
+    source2.pipe(toArray()).subscribe((res) => {
+      // console.log(res);
     })
 
-    
+
     //toArray (Example 03)
-    const source3 = of("Apple","banana","Kiwi");
-    source3.pipe(toArray()).subscribe((res)=>{
-      console.log(res);
+    const source3 = of("Apple", "banana", "Kiwi");
+    source3.pipe(toArray()).subscribe((res) => {
+      // console.log(res);
     })
-    
-    //of()
-    const fruits = of("Apple","banana","Kiwi");
-    
-    fruits.subscribe((res)=>{console.log(res)})
-  }
-  // fromEvent(button,'click')
-  
-  ngAfterViewInit(){
-    let count=1;
-    fromEvent(this.addBtn.nativeElement,'click')
-    .subscribe((res)=>{
-      console.log(res);
-      let countval= 'add '+ count++;
-      this._rxjsPrac.printEl(countval,'elContainer1');
-      this._rxjsPrac.printEl(countval,'elContainer2');
 
+    // interval (Example 01)
+    const broadCast = interval(2000);
+    broadCast.subscribe((res) => {
+      // console.log(res);
+      this.obsMsg = res;
     })
+
+    //for timer
+    const broadCast2 = timer(5000, 1000);
+    broadCast2.subscribe((res) => {
+      // console.log("Timer : "+res);
+    })
+
+    //of()
+    const fruits = of("Apple", "banana", "Kiwi");
+
+    fruits.subscribe((res) => { console.log(res) })
   }
+
+  ngAfterViewInit() {
+    // fromEvent(button,'click')
+    let count = 1;
+    fromEvent(this.addBtn.nativeElement, 'click')
+      .subscribe((res) => {
+        // console.log(res);
+        let countval = 'add ' + count++;
+        this._rxjsPrac.printEl(countval, 'elContainer1');
+        this._rxjsPrac.printEl(countval, 'elContainer2');
+
+      })
+  }
+
+  ngOnDestroy(): void {
+  console.log("destroyed");
+    this.subs2.unsubscribe();
+  }
+
 
   // printEl(count : any, elContainer : string){
   //   let el=document.createElement('li');
